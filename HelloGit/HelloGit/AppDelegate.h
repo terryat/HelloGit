5d9135e3f79ba8ff404662919b8d05ee339dc39a
Penghui Zheng <penghuiat@gmail.com>
2015-03-23 22:20:16 +0800

    Initial Version

---
+++ HelloGit/HelloGit/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  HelloGit
+//
+//  Created by iammac on 15/3/23.
+//  Copyright (c) 2015年 nX Software Studio. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ HelloGit/HelloGit/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  HelloGit
+//
+//  Created by iammac on 15/3/23.
+//  Copyright (c) 2015年 nX Software Studio. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
